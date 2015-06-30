c29691f293a5f52bd99517acd9ac5ad6a9f8962f
kiwenlau <kiwenlau@126.com>
2015-06-30 12:34:06 +0900

    get all push and pull log

--- analyzelog.py
+++ analyzelog.py
@@ -2,29 +2,16 @@
 
 f=open("axle-base.txt", "r")
 
-# for push i%2==0; for pull i%2==1
-i=0
+line=f.readline()
+while line :
+    if 'GET /v2/ HTTP/1.1' in line :
+        fpush=open("push-log.txt", "a");
+        while line :
+            fpush.write(line)
+            if ('PUT /v1/repositories/' in line) and ('images' in line) or (line == "\n"):
+                fpush.write("\n")
+                break
+            else :
+                line=f.readline()
+    line=f.readline()
 
-# get the push log from the original log
-while True:
-	line=f.readline()
-	if line:
-		if 'GET /v2/ HTTP/1.1' in line :
-			if i%2==0 :
-				fpush=open('push-log.txt', 'w')
-				fpush.write(line)
-				line=f.readline()
-				while not (('PUT /v1/repositories/' in line) and ('images' in line)) :
-					line=f.readline()
-					fpush.write(line)
-				fpush.close()
-				break
-				print 'push'
-			else :
-				print 'pull'
-			i+=1
-	else:
-		break
-
-f.close()
-		
