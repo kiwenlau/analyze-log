59b72d38f5bfd18c53fffb8de40d8828bcf26e18
kiwenlau <kiwenlau@126.com>
2015-06-30 12:44:14 +0900

    add comment

--- analyzelog.py
+++ analyzelog.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 
+# get all push and pull log
+# write all push and pull log into "push-pull-log.txt"
+
 f=open("axle-base.txt", "r")
 
 line=f.readline()
 while line :
+    # the begin of push log or pull log
     if 'GET /v2/ HTTP/1.1' in line :
-        fpush=open("push-log.txt", "a");
+        fpushpull=open("push-pull-log.txt", "a");
         while line :
-            fpush.write(line)
+            fpushpull.write(line)
+            # the end of push log or pull log
             if ('PUT /v1/repositories/' in line) and ('images' in line) or (line == "\n"):
-                fpush.write("\n")
+                fpushpull.write("\n")
                 break
             else :
                 line=f.readline()
     line=f.readline()
 
+f.close()
+fpushpull.close()
