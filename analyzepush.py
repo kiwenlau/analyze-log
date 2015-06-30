38fe0b2dc1c959161edf10db36cff8bac0b03cc5
kiwenlau <kiwenlau@126.com>
2015-06-30 18:34:49 +0900

    refine

--- analyzepush.py
+++ analyzepush.py
@@ -8,22 +8,18 @@ def timeToSeconds(timeStr) :
 	hour=float(timeStr[11:13])
 	minute=float(timeStr[14:16])
 	second=float(timeStr[17:29])
-	#print timeStr[8:10], timeStr[11:13], timeStr[14:16], timeStr[17:29]
-	#print day, hour, minute, second
 	return day*86400+hour*3600+minute*60+second;
 
 def calculatePushTime(timeString = [], *args) :
     # transfer timestamp to second
     timeSecond = []
     for k, v in enumerate(timeString):
-        #print k, v
         timeSecond.append(timeToSeconds(timeString[k]))
-        #print '%.9f' % timeToSeconds(timeString[k])
     layerNumber=(len(timeString)-2)/5
     totalPushTime = timeSecond[len(timeString)-1]-timeSecond[0]
     print totalPushTime
-    #checkTime = timeSecond[2*layerNumber] - timeSecond[0]
-    #print checkTime
+    checkTime = timeSecond[2*layerNumber] - timeSecond[0]
+    print checkTime
     return 
 
 timeString = []
@@ -31,7 +27,6 @@ timeString = []
 line=f.readline()
 while line :
     while line != "\n" :
-        
         # push begin
         # check whether image layers exist in the registry
         if "GET /v2/ HTTP/1.1" in line :
@@ -46,12 +41,10 @@ while line :
         if (('PUT /v1/repositories/' in line) and ('images' in line)) :
             timeString.append(line[:29])
         line=f.readline()
-            #for k, v in enumerate(timeString):
-#print k, v
     calculatePushTime(timeString)
     timeString = []
     line=f.readline()
-#break
+    #break
 
 f.close()
 		
--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ loglist=(axle-base.txt gocd-server.txt)
 
 for logname in ${loglist[*]};
 do
-    rm push-pull-log.txt push-logs.txt pull-logs.txt 
+    rm push-pull-log.txt push-logs.txt pull-logs.txt &> /dev/null
 	./analyzelog.py logs/$logname
     ./separatepushpull.py
     ./analyzepush.py
 done
 
-rm push-pull-log.txt push-logs.txt pull-logs.txt
+#rm push-pull-log.txt push-logs.txt pull-logs.txt
