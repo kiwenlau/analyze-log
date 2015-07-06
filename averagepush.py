90464aaf4e868c2fa66618c3c62e926cda867258
kiwenlau <kiwenlau@126.com>
2015-07-06 16:37:46 +0900

    calculate average transfering time for layer and checksum

--- averagepush.py
+++ averagepush.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+# for each step of push, calculate the average time of 10 execution output
+# omit the 2 largest and 2 smallest output
+
 import sys
 import numpy
 
 
-# cal
+# calculate the layer number 
+# calculate the index of consecutive execution output
+# read the whole files
 f=open(sys.argv[1], "r")
 line = f.readline().rstrip("\n")
 layerNumber = int(line[16:])
@@ -13,11 +18,10 @@ lines = f.readlines()
 f.close()
 
 
-
 totalTime = []
 checkTime = []
 
-# get all total push time
+# get all total push time and check time
 for i in range(0, 9) :
     total=float(lines[i*index].rstrip("\n"))
     #print total
@@ -37,6 +41,7 @@ min2=sorted(range(len(totalTime)), key=lambda i: totalTime[i])[:2]
 #print max2
 #print min2
 
+# trim is the index of omitted output
 trim = max2 + min2
 
 
@@ -44,14 +49,18 @@ def averageTime (Time, trim) :
     for i in trim :
         Time[i] = 0
     #print Time
-    return reduce(lambda x, y: x + y, Time) / (len(Time)-4)
+    return reduce(lambda x, y: x + y, Time) / (len(Time)- len(trim))
 
+# calculate the average total time
 averageTotalTime = averageTime(totalTime, trim)
 print averageTotalTime
 
+# calculate the average check time
 averageCheckTime = averageTime(checkTime, trim)
 print averageCheckTime
 
+
+# calculate the average time for transforing the json file for each layers
 jsonMatrix = numpy.zeros((6, layerNumber))
 
 reserved = [x for x in range(10) if x not in trim ]
@@ -80,6 +89,28 @@ for x in averageJsonTime :
     print x
 
 
+# calculate the average time for transforing json, layer and checksum of all layers
+def averageLayerTime(offset) :
+    timeMatrix = numpy.zeros((6, layerNumber))
+    reserved = [ x for x in range(10) if x not in trim ]
+    j = 0
+    for i in reserved :
+        timeMatrix[j] = lines[(offset + i*index) : (offset + i*index + layerNumber)]
+        j = j + 1
+    #print timeMatrix
+    timeMatrix2 = numpy.transpose(timeMatrix) 
+    averageTime = []
+    for i in range(layerNumber) :
+        average = reduce(lambda x, y : x + y, timeMatrix2[i]) / 6
+        averageTime.append(average)
+    print "\n"
+    for x in averageTime:
+        print x
+
+averageLayerTime(4)
+averageLayerTime(6+layerNumber)
+averageLayerTime(8+layerNumber*2)       
+    
 
 
 
